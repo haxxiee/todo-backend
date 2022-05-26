68d2e507529cacf7c0035f587deec38437105d16
Linus Foxell <linusfoxell@hotmail.com>
2022-05-26 17:54:21 +0200

    small fixes

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ const {
 
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Credentials", "true");
-  // res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  // );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PATCH, DELETE, OPTIONS, POST, PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
 
   if (req.url === "/todos" && req.method === "GET") {
     getAllTodos(res);
